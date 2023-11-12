93d977411c565ee88cdb3ad08129db3d14101e91
JackLandgraff <jacklandgraff2024@u.northwestern.edu>
2023-11-12 15:18:07 -0800

    huh

--- loading quizzes.R
+++ loading quizzes.R
@@ -4,3 +4,16 @@ learnr::run_tutorial("quiz-1", package = "r4dsQuizzes")
 
 library(tidyverse)
 glimpse(bikeshare)
+
+library(palmerpenguins)
+
+write_rds(penguins, "data/penguins_clean.rds")
+
+
+remotes::install_github("NUstat/r4dsQuizzes", dependencies = TRUE)
+
+
+learnr::run_tutorial("quiz-2", package = "r4dsQuizzes")
+1
+
+remotes::install_github("NUstat/r4dsQuizzes", dependencies = TRUE)
